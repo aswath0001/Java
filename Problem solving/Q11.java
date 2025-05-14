d40b5cf079b92e048e3c19605701156bfaf4a563
aswath0001 <rsaswaths38@gmail.com>
2025-05-14 18:38:06 +0530

    new challenges

--- Problem solving/Q11.java
+++ Problem solving/Q11.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Q11 {
     /*
-     * fibonacci series
+     * fibonacci series in java
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
